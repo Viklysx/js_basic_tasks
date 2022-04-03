a871beeb67d254d7caed6d3ce4b802033ceb6c34
Victoriya <vikbelec111@yandex.ru>
2022-04-03 18:07:43 +0300

    fix 6.js

--- 6.js
+++ 6.js
@@ -1,14 +1,4 @@
-function countBs(str) {
-    let countB = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (str.charAt(i) === "B") countB++;
-    }
-    return countB;
-}
-
-console.log(countBs('bbBBBssbBBBB'));
-
-function countChar(str, symbol) {
+function countBs(str, symbol) {
     let countB = 0;
     for (let i = 0; i < str.length; i++) {
         if (str.charAt(i) === symbol) countB++;
@@ -16,4 +6,4 @@ function countChar(str, symbol) {
     return countB;
 }
 
-console.log(countChar('bbBBBssbBBBB', 's'));
+console.log(countBs('bbBBBssbBBBBs', 's'));
