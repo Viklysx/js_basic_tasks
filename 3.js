79ae9d8a44281bc68d01410d734ffaebbd350b82
Victoriya <vikbelec111@yandex.ru>
2022-04-03 03:46:51 +0300

    fix 3.js

--- 3.js
+++ 3.js
@@ -1,5 +1,5 @@
 function chessBoard(rows = 8, cols = 8) {
-let board = '';
+    let board = '';
     let symbolChess = '';
     for (let i = 1; i <= rows; i++) {
         for (let j = 1; j <= cols; j++) {
