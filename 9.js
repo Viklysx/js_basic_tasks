c6f14b9d350acff7b9c444c9afc30a3a2fb82ed6
Viklysx <vikbelec111@yandex.ru>
2022-04-07 20:37:48 +0300

    the solution is supplemented in 9.js

--- 9.js
+++ 9.js
@@ -1,4 +1,4 @@
-function arrayToList(array){
+function arrayToList(array) {
     return {
         value: array.shift(),
         rest: (array.length !== 0) ? arrayToList(array) : null
@@ -15,11 +15,36 @@ function listToArray(list) {
     return array;
 }
 
+function prepend(element, list) {
+    return {
+        value: element,
+        rest: list
+    }
+}
+
+function nth(list, position) {
+    // 1-й вариант решения
+
+    // let array = listToArray(list);
+    // if (array[position]) return array[position]
+    // else return undefined;
+
+    // 2-й вариант
+
+    if (position > 0 && list.rest) {
+        position--;
+        list = list.rest;
+        return nth(list, position); 
+    } else if (position === 0) {
+        return list.value;
+    } else return undefined;
+}
+
 console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
 console.log(listToArray(arrayToList([10, 20, 30])));
-// // → [10, 20, 30]
-// console.log(prepend(10, prepend(20, null)));
-// // → {value: 10, rest: {value: 20, rest: null}}
-// console.log(nth(arrayToList([10, 20, 30]), 1));
-// // → 20
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+//→ {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
